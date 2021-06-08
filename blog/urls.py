8030bd9348477da22406cda59efe63ae21238ad1
DjangodevArif <arifanytest@gmail.com>
2021-06-08 15:25:32 +0600

    [ADD] top-rated-category and category api

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('<int:pk>/', PostDetail.as_view(), name='singleapi'),
     path('<int:pk>/comments/', CommentList.as_view(), name='comment'),
     path('catelog/', CategoryList.as_view(), name='catogory'),
+    path('most_popular/', TopRatedCategory.as_view(), name='popular_category'),
+    path('<title>/', CategoryView.as_view(), name='category_view'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
-from rest_framework import generics, permissions
+from django.db.models.aggregates import Max
+from django.http import response
+from django.db.models import Count
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import *
 from .serializers import *
@@ -34,3 +39,33 @@ class CommentList(generics.ListCreateAPIView):
 class CategoryList(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializers
+
+
+class TopRatedCategory(APIView):
+
+    def get(self, request):
+        all = Category.objects.annotate(
+            entries=Count('post'))
+        post = Category.objects.annotate(
+            entries=Count('post')).aggregate(Max('entries'))
+        get = all.filter(entries=post['entries__max'])
+        serialize = CategorySerializers(
+            get, many=True, context={'request': request})
+        return Response(data=serialize.data, status=status.HTTP_200_OK)
+
+
+class CategoryView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        kwarg = kwargs.get('title')
+        category = generics.get_object_or_404(Category, title__icontains=kwarg)
+        print('category', category)
+        serialize = CategorySerializers(
+            category, context={'request': request})
+        return Response(data=serialize.data, status=status.HTTP_200_OK)
+
+
+'''    
+q = Category.objects.annotate(entries=Count('post')).aggregate(Max('entries'
+))
+'''
