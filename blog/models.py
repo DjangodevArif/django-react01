ee8850a3b03609575f9bacb79393b46a19c0e4e3
DjangodevArif <arifanytest@gmail.com>
2021-07-01 23:22:21 +0600

    [ADD]like,registration,image url,subcomment with all request

--- blog/models.py
+++ blog/models.py
@@ -12,15 +12,10 @@ class Category(models.Model):
         return self.title
 
 
-def post_img_path(instance, filename):
-    return 'post_pics/{0}/{1}'.format(instance.post_title, filename)
-
-
 class Post(models.Model):
 
     post_author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_img = models.ImageField(
-        default='post_pic.jpg', upload_to=post_img_path)
+    post_img = models.URLField(max_length=500, null=True)
     post_title = models.CharField(max_length=250)
     post_detail = models.TextField(max_length=1000)
     post_date = models.DateTimeField(auto_now_add=True)
@@ -40,3 +35,13 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.post_id.post_title} by {self.comment_author.username}'
+
+
+class SubComment(models.Model):
+    main_comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    comment_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_detail = models.TextField(max_length=150)
+    comment_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Subcomment by {self.comment_author}'
--- blog/urls.py
+++ blog/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
 
 from .views import *
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 
 urlpatterns = [
     path('', PostList.as_view(), name='homeapi'),
     path('<int:pk>/', PostDetail.as_view(), name='singleapi'),
-    path('<int:pk>/comments/', CommentList.as_view(), name='comment'),
+    path('like/', LikeView.as_view(), name='likeapi'),
+    path('comments/', CommentList.as_view(), name='comment'),
+    path('subcomments/', SubCommentList.as_view(), name='comment'),
     path('catelog/', CategoryList.as_view(), name='catogory'),
+    path('com_sub/<model>/<int:pk>/',
+         CommSubView.as_view(), name='comment_subcomment'),
     path('most_popular/', TopRatedCategory.as_view(), name='popular_category'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('verify/', TokenVerifyView.as_view(), name='token_verify'),
-    # path('verify/', VerifyTokenView.as_view(), name='token_verify'),
+    path('new_user/', UserCreateView.as_view(), name='new_user'),
     path('<title>/', CategoryView.as_view(), name='category_view'),
 
 ]
